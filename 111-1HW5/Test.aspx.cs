2d2e49d01784f7502f3cafdd302cde9bc30b35cd
35596035 <william82733791@gmail.com>
2022-12-06 21:13:41 +0800

    Finish

--- 111-1HW5/Test.aspx.cs
+++ 111-1HW5/Test.aspx.cs
@@ -15,7 +15,25 @@ namespace _111_1HW5
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            string s_Str = ConfigurationManager.ConnectionStrings["SQLLOCALDB"].ConnectionString;
+            if (!IsPostBack)
+            {
+                try
+                {
+                    SqlConnection o_Str = new SqlConnection(s_Str);
+                    o_Str.Open();
+                    SqlDataAdapter o_A = new SqlDataAdapter("Select * from Users", o_Str);
+                    DataSet o_D = new DataSet();
+                    o_A.Fill(o_D, "ds_Res");//SQL轉接器物件名稱.Fill(資料集物件名稱, 資料表名稱);
+                    gd_View.DataSource = o_D;
+                    gd_View.DataBind();
+                    o_Str.Close();
+                }
+                catch (Exception o_ex)
+                {
+                    Response.Write(o_ex.ToString());
+                }
+            }
         }
 
         protected void btn_Insert_Click(object sender, EventArgs e)
@@ -26,23 +44,17 @@ namespace _111_1HW5
                 SqlConnection o_Str = new SqlConnection(s_Str);
                 SqlDataAdapter o_A = new SqlDataAdapter("Select * from Users", o_Str);
                 o_Str.Open();
-                SqlCommand o_cmd = new SqlCommand("Insert into Users (Id, Name, Birthday)" + "values(@Id, @Name, @DateTime)", o_Str);
-                //o_cmd.ExecuteNonQuery();
-                o_cmd.Parameters.Add("@Id", SqlDbType.Int);
-                o_cmd.Parameters["@Id"].Value = 3;
+                SqlCommand o_cmd = new SqlCommand("Insert into Users (Name, Birthday)" + "values(@Name, @DateTime)", o_Str);
+                //o_cmd.Parameters.Add("@Id", SqlDbType.Int);
+                //o_cmd.Parameters["@Id"].Value = 3;
                 o_cmd.Parameters.Add("@Name", SqlDbType.NVarChar, 50);
                 o_cmd.Parameters["@Name"].Value = "阿貓阿狗";
                 o_cmd.Parameters.Add("@DateTime", SqlDbType.DateTime);
                 o_cmd.Parameters["@DateTime"].Value = "2000/10/10";
-                //o_cmd.ExecuteNonQuery();
-                o_A.InsertCommand = o_cmd;
-                //int i_Flag = o_cmd.ExecuteNonQuery();
-                //Response.Write(i_Flag);
-
-                DataSet o_D = new DataSet();
-                o_A.Fill(o_D, "ds_Res");//SQL轉接器物件名稱.Fill(資料集物件名稱, 資料表名稱);
-                gd_View.DataSource = o_D;
-                gd_View.DataBind();
+                o_cmd.ExecuteNonQuery();
+                //o_A.InsertCommand = o_cmd;
+                Response.Redirect("https://localhost:44393/Test.aspx", false);
+                HttpContext.Current.ApplicationInstance.CompleteRequest();
                 o_Str.Close();
             }
             catch (Exception o_ex)
